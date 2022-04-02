5238b7687b1537b45844098526c88b2575145325
leenu123 <leenu.thampy@gmail.com>
2022-04-02 12:09:50 +0530

    Greens site examples

---
+++ GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,39 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		// TODO Auto-generated method stub
+		System.out.println("GreensOmr");
+	}
+
+	private void greensAdayar() {
+		// TODO Auto-generated method stub
+		System.out.println("greensAdayar");
+	}
+
+	private void greenTambaram() {
+		// TODO Auto-generated method stub
+		System.out.println("greenTambaram");
+	}
+
+	private void greensVelachery() {
+		// TODO Auto-generated method stub
+		System.out.println("greensVelachery");
+	}
+
+	private void greenAnnaNagar() {
+		// TODO Auto-generated method stub
+		System.out.println("greenAnnaNagar");
+	}
+
+	public static void main(String[] args) {
+		GreensTech greensLoc = new GreensTech(); 
+		greensLoc.greensOmr();
+		greensLoc.greensAdayar();
+		greensLoc.greenTambaram();
+		greensLoc.greensVelachery();
+		greensLoc.greenAnnaNagar();
+		
+		
+	}
+}
